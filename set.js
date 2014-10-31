d5c33c7a0f097decc0996b179f8b585a3851f0e9
Nikolay Redko <nredko@nredko.com>
2014-10-31 19:03:22 +0600

    .

--- set.js
+++ set.js
@@ -11,6 +11,7 @@ Array.prototype.shuffle = function() {
 var cards = [];
 var Count = 12;
 var SelCount = 0;
+var LastHint = [0, 0, 0];
 var _Figure = ['diamond', 'oval', 'squiggle'];
 var _Style = ['solid', 'striped', 'open'];
 var _Color = ['blue', 'green', 'red'];
@@ -39,6 +40,7 @@ function init(){
                     cards[i].color = color;
                     cards[i].count = cnt;
                     cards[i].selected = false;
+                    cards[i].hint = false;
                     cards[i].html = card_html(fig, style, color, cnt);
                     i++;
                 }
@@ -46,24 +48,116 @@ function init(){
         }
     }
     cards = cards.shuffle();
+    cards.splice(12);
 }
-function go(elem){
-    ind = $(elem).attr('i');
-    console.log(ind);
-    cards[ind].selected = !cards[ind].selected;
-    if(cards[ind].selected){
-        $(elem).addClass("not_selected");
-        $(elem).removeClass("selected");
-        SelCount--;
+
+function clear(){
+    for(var i = 0; i < cards.length; i++){
+        cards[i].selected = false;
+        cards[i].hint = false;
     }
-    else {
-        $(elem).removeClass("not_selected");
-        $(elem).addClass("selected");
-        SelCount++;
-        if(SelCount == 3){
-            Check();
+    SelCount = 0;
+    LastHint = [0,0,0];
+}
+function triple(v1, v2, v3){
+    return ((v1 == v2 && v2 == v3) || (v1 != v2 && v2!= v3 && v1 != v3));
+}
+
+function is_set(card1, card2, card3){
+    return triple(card1.fig, card2.fig, card3.fig) &&
+           triple(card1.color, card2.color, card3.color) &&
+           triple(card1.style, card2.style, card3.style) &&
+           triple(card1.count, card2.count, card3.count);
+}
+
+function check(){
+    var cnt = Math.min(Count, cards.length);
+    var selected = [];
+    for(var i = 0; i < cnt; i++){
+        if(cards[i].selected){
+            cards[i].num = i;
+            selected.push(cards[i]);
         }
     }
+    if(selected.length == 3 && is_set(selected[0], selected[1], selected[2])){
+        showAlert("Правильно!");
+        cards.splice(selected[2].num, 1);
+        cards.splice(selected[1].num, 1);
+        cards.splice(selected[0].num, 1);
+        clear();
+        if(Count > 12)
+            Count -=3;
+        draw();
+        if(!hint(false) && cards.length <= 12){
+             showAlert("Больше ходов нет!");
+        }
+    } else {
+        showAlert("Неправильно! Это не СЕТ.");
+    }
+}
+
+function hint(need_draw){
+    clear();
+    var cnt = Math.min(Count, cards.length);
+    for(i = LastHint[0]; i < cnt; i++){
+        for(j = Math.max(i + 1, LastHint[1]); j < cnt; j++){
+            for(k = Math.max(j + 1, LastHint[2]); k < cnt; k++){
+                if(is_set(cards[i], cards[j], cards[k])) {
+                    cards[i].hint = true;
+                    cards[j].hint = true;
+                    cards[k++].hint = true;
+                    if(k == cnt){
+                        k = 0;
+                        j++;
+                    }
+                    if(j == cnt){
+                        j = 0;
+                        i++;
+                    }
+                    if(i == cnt){
+                        i = 0;
+                    }
+                    if(need_draw){
+                        LastHint = [i, j, k];
+                        draw();
+                    }
+                    return true;
+                }    
+            }
+            LastHint[2] = 0;
+        }
+        LastHint[1] = 0;
+    }
+    LastHint[0] = 0;
+    return false;
+}
+
+function showAlert(str){
+    $("#alert").removeClass("hidden");
+    $("#alert").text(str);
+    setTimeout(function(){$("#alert").addClass("hidden");}, 5000);
+}
+
+function go(elem){
+    LastHint = 0;
+    showAlert("");
+    ind = $(elem).attr('i');
+    cards[ind].selected = !cards[ind].selected;
+    if(cards[ind].selected){
+        if(SelCount == 3){
+            clear();
+            cards[ind].selected = true;
+        }
+        SelCount++;
+        if(SelCount == 3){
+            check();
+        }
+    }
+    else {
+        SelCount--;
+    }
+    draw();
+
 }
 
 function draw(){
@@ -77,6 +171,11 @@ function draw(){
             if(i < cards.length){
                 td = $(cards[i].html);
                 td.attr('i', i);
+                if(cards[i].selected){
+                    td.addClass('selected');
+                } else if(cards[i].hint){
+                    td.addClass('hint');
+                }
                 td.click(function(){
                     go(this);
                 });
@@ -94,7 +193,11 @@ $(document).ready(function(){
     $("#next").click(function(){
         if(Count < 21){
             Count += 3;
-    }
-    draw();
+            clear();
+        }
+        draw();
+    });
+    $("#hint").click(function(){
+        hint(true);
     });
 });
